deff5b525c0b2e182b885d9ac1df23221e337c21
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-23 19:59:55 +0200

    create simple views for Task

--- backend/square_check/views.py
+++ backend/square_check/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from square_check.models import Task
+from square_check.serializers import TaskSerializer
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+class TaskList(APIView):
+
+    def get(self, request, format=None):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+
+class TaskDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
