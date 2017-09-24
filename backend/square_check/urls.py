03844a6abd4d8596f5fae4ccf4b2221f5012a588
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-24 17:28:33 +0200

    add urls for List

--- backend/square_check/urls.py
+++ backend/square_check/urls.py
@@ -4,7 +4,9 @@ from square_check import views
 
 urlpatterns = [
     url(r'^tasks/$', views.TaskList.as_view()),
-    url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view())
+    url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view()),
+    url(r'^lists/$', views.ListList.as_view()),
+    url(r'^lists/(?P<pk>[0-9]+)/$', views.ListDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
