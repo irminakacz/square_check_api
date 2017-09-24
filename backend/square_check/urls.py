62e70af9148576d5a13d77be86781bd230c81710
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-24 20:08:07 +0200

    add User serializer and router path

--- backend/square_check/urls.py
+++ backend/square_check/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'tasks', views.TaskViewSet)
 router.register(r'lists', views.ListViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- backend/square_check/views.py
+++ backend/square_check/views.py
@@ -1,6 +1,7 @@
 from square_check.models import Task, List
-from square_check.serializers import TaskSerializer, ListSerializer
+from square_check.serializers import TaskSerializer, ListSerializer, UserSerializer
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -8,8 +9,11 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
 
 
-class ListViewSet(viewsets.ReadOnlyModelViewSet):
+class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
 
 
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
