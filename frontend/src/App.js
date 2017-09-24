e20a5a0f5dac1de59c8e4e8abd92faf6973020e4
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-24 19:25:35 +0200

    change fetching all tasks to fetching all lists
    Instead of fetching all the tasks from backend, now the App.js is fetching all
    the lists (which contain all their tasks) and passes the tasks to each list as a
    prop.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,48 @@ import List from './List';
 
 class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount() {
+    let lists = [];
+    this.props.conn.get('/lists').then(response => {
+
+      response.data.forEach(list => {
+        lists.push({
+          id: list.id,
+          title: list.title, 
+          color: list.color,
+          tasks: list.tasks
+        });
+      })
+
+      this.setState({
+        lists: lists
+      });
+    });
+
+  }
+
   render() {
+    const lists = this.state.lists.map(list => {
+      return (
+        <List 
+          key={ "list_" + list.id } 
+          list={ list } 
+          conn={ this.props.conn }
+        />
+      );
+    });
     return (
-      <List conn={this.props.conn} />
+      <div>
+        { lists }
+      </div>
     );
   }
 }
--- frontend/src/List.js
+++ frontend/src/List.js
@@ -4,41 +4,14 @@ import './List.css';
 
 class List extends Component {
 
-  constructor() {
-    super();
-
-    this.state = {
-      tasks: []
-    }
-  }
-
-  componentDidMount() {
-    let tasks = [];
-    this.props.conn.get('/tasks').then(response => {
-
-      response.data.forEach(task => {
-        tasks.push({
-          id: task.id,
-          task: task.task, 
-          done: task.done
-        });
-      })
-
-      this.setState({
-        tasks: tasks
-      });
-    });
-
-  }
-
   render() {
-    const tasks = this.state.tasks.map(task => {
+    const tasks = this.props.list.tasks.map(task => {
       return <Task key={ "task_" + task.id } task={ task } />;
     });
 
     return (
       <div className="List">
-        <div id="list-title">List</div>
+        <div id="list-title">{ this.props.list.title }</div>
         <div id="list-tasks">
           { tasks }
         </div>
