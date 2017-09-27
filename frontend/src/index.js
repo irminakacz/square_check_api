abef3ea1c09e61471989e63b4355d0305dc90b3b
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-27 20:24:10 +0200

    split repository by removing frontend

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import axios from 'axios';
-
-const conn = axios.create({ baseURL: 'http://localhost:8000' });
-
-ReactDOM.render(<App conn={conn} />, document.getElementById('root'));
